fa3a4c9fc753cbdf36b28cc45ca38969302d20de
aravind4799 <araviku04@gmail.com>
2020-08-05 10:34:29 +0530

    setting up server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/",(req,res) =>{
+    res.sendFile(__dirname + "/index.html")
+});
+
+
+app.listen(process.env.PORT || 3000,()=>("server up and running"))
